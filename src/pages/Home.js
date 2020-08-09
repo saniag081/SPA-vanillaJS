17ad1ebea918ae19e325656bd771dd94887dafd2
saniag081 <sgonzalezhernandez081@gmail.com>
2020-08-08 22:51:03 -0500

    template home

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+function Home() {
+	const view = `
+		<div class="characters">
+			<article class="characters-item">
+				<a href="#/1/">
+					<img src="" alt="" />
+					<h2></h2>
+				</a>
+			</article>
+		</div>
+	`
+	return view;
+}
+
+export default Home;
