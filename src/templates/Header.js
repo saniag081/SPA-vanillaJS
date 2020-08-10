cff2f4746893d9d8996e28a4d49835eed91c0c10
saniag081 <sgonzalezhernandez081@gmail.com>
2020-08-09 21:06:36 -0500

    template header

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+function Header() {
+	const view = `
+		<header class="header-main">
+			<div class="header-logo">
+				<h1>
+					<a href="/">
+						100tifi.co
+					</a>
+				</h1>
+			</div>
+			<nav class="hrader-nav">
+				<a href="#/about/">About</a>
+			</nav>
+		</header>
+	`;
+	return view;
+};
+
+export default Header;
